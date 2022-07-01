8d54f666294c28de3575ec9996982246362e3ba5
ababu alemu <ababualemu123@gmail.com>
2022-07-01 11:01:50 +0300

    minor changes

--- config/database.php
+++ config/database.php
@@ -43,9 +43,27 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        // 'mysql' => [
+        //     'driver' => 'mysql',
+        //     'url' => env('MYSQL_ADDON_URI'),
+        //     'host' => env('MYSQL_ADDON_HOST', '127.0.0.1'),
+        //     'port' => env('MYSQL_ADDON_PORT', '3306'),
+        //     'database' => env('MYSQL_ADDON_DB', 'forge'),
+        //     'username' => env('MYSQL_ADDON_USER', 'forge'),
+        //     'password' => env('MYSQL_ADDON_PASSWORD', ''),
+        //     'unix_socket' => env('DB_SOCKET', ''),
+        //     'charset' => 'utf8mb4',
+        //     'collation' => 'utf8mb4_unicode_ci',
+        //     'prefix' => '',
+        //     'prefix_indexes' => true,
+        //     'strict' => true,
+        //     'engine' => null,
+        //     'options' => extension_loaded('pdo_mysql') ? array_filter([
+        //         PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+        //     ]) : [],
+        // ],
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('MYSQL_ADDON_URI'),
             'host' => env('MYSQL_ADDON_HOST', '127.0.0.1'),
             'port' => env('MYSQL_ADDON_PORT', '3306'),
             'database' => env('MYSQL_ADDON_DB', 'forge'),
@@ -55,12 +73,8 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
         ],
 
         'pgsql' => [
