5676cdf6c2f36ea9cd28b597ae70e049216ede16
ababu alemu <ababualemu123@gmail.com>
2022-07-02 11:48:05 +0300

    model and migration files created

---
+++ app/Models/TransportInfo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TransportInfo extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2022_07_02_080921_create_patient_infos_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patient_infos', function (Blueprint $table) {
+            $table->id();
+            $table->string('parent_name');
+            $table->date('birthday');
+            $table->string('region');
+            $table->string('woreda');
+            $table->string('child_name');
+            $table->integer('age');
+            $table->string('sex');
+            $table->string('education');
+            $table->string('diagnose');
+            $table->date('entrance_date');
+            $table->boolean('is_active');
+            $table->string('center_location');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patient_infos');
+    }
+};
